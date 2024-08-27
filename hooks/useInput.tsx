ae77d8a1032f7c453ef8b2889febb9d478a06e57
awebcode <asikurrahaman997@gmail.com>
2024-08-27 21:40:39 +0600

    nextjs-multistep-form-zod added metatags v3

--- hooks/useInput.tsx
+++ hooks/useInput.tsx
@@ -1,39 +1,38 @@
-// useInput.ts
-import { useState } from 'react';
-import { z, ZodError } from 'zod';
-import { schema, SchemaType } from '../schema/validationSchema';
+// // useInput.ts
+// import { useState } from 'react';
+// import { z, ZodError } from 'zod';
 
-type UseInputReturn<T> = {
-    value: T;
-    error: string | null;
-    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-    validate: () => void;
-};
+// type UseInputReturn<T> = {
+//     value: T;
+//     error: string | null;
+//     onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+//     validate: () => void;
+// };
 
-export default function useInput<T extends keyof SchemaType>(field: T): UseInputReturn<SchemaType[T]> {
-    const [value, setValue] = useState<SchemaType[T]>(schema.shape[field].safeParse('').data || '' as SchemaType[T]);
-    const [error, setError] = useState<string | null>(null);
+// export default function useInput<T extends keyof SchemaType>(field: T): UseInputReturn<SchemaType[T]> {
+//     const [value, setValue] = useState<SchemaType[T]>(schema.shape[field].safeParse('').data || '' as SchemaType[T]);
+//     const [error, setError] = useState<string | null>(null);
 
-    const validate = () => {
-        try {
-            schema.shape[field].parse(value);
-            setError(null);
-        } catch (err) {
-            if (err instanceof ZodError) {
-                setError(err.issues[0].message);
-            }
-        }
-    };
+//     const validate = () => {
+//         try {
+//             schema.shape[field].parse(value);
+//             setError(null);
+//         } catch (err) {
+//             if (err instanceof ZodError) {
+//                 setError(err.issues[0].message);
+//             }
+//         }
+//     };
 
-    const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setValue(event.target.value as SchemaType[T]);
-        validate();
-    };
+//     const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+//         setValue(event.target.value as SchemaType[T]);
+//         validate();
+//     };
 
-    return {
-        value,
-        error,
-        onChange,
-        validate,
-    };
-}
+//     return {
+//         value,
+//         error,
+//         onChange,
+//         validate,
+//     };
+// }
