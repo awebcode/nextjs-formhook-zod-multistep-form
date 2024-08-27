7343c2fa63d3fa0e76f1c1f2b979762a598fd38c
awebcode <asikurrahaman997@gmail.com>
2024-08-27 21:30:23 +0600

    nextjs-multistep-form-zod v3

--- components/multistep/Stepper.tsx
+++ components/multistep/Stepper.tsx
@@ -11,8 +11,8 @@ const Stepper: React.FC<StepperProps> = ({ steps, currentStep, setcurrentStep, v
     return (
         <div className="flex flex-col items-center mb-8">
             {/* Current Step Indicator */}
-            <div className="text-sm text-gray-600 mb-4">
-                Step {currentStep + 1} of {steps.length}
+            <div className="text-sm text-violet-600 mb-4">
+                Step {currentStep + 1} / {steps.length}
             </div>
 
             {/* Step Circles and Labels */}
@@ -20,20 +20,20 @@ const Stepper: React.FC<StepperProps> = ({ steps, currentStep, setcurrentStep, v
                 {steps.map((step, index) => (
                     <div key={index} className="flex items-center cursor-pointer" onClick={() => setcurrentStep(index)}>
                         <div
-                            className={`w-10 h-10 rounded-full flex items-center justify-center transition-colors duration-300 ease-in-out ${validationStatus[index] ? 'bg-indigo-600 text-white' : 'bg-gray-300 text-gray-500'
+                            className={`w-10 h-10 rounded-full flex items-center justify-center transition-colors duration-300 ease-in-out ${validationStatus[index] ? 'bg-violet-600 text-white' : 'bg-gray-300 text-gray-500'
                                 }`}
                         >
                             {validationStatus[index] ? <span className="text-white text-xl">✓</span> : index + 1}
                         </div>
                         <span
-                            className={`ml-2 transition-colors duration-300 ease-in-out ${validationStatus[index] ? 'text-indigo-600 ' : 'text-gray-500'
+                            className={`ml-2 transition-colors duration-300 ease-in-out ${validationStatus[index] ? 'text-violet-600 ' : 'text-gray-500'
                                 }`}
                         >
                             {step}
                         </span>
                         {index < steps.length - 1 && (
                             <div className="flex-1 border-t-2 transition-all duration-300 ease-in-out ${
-                                index < currentStep ? 'border-indigo-600' : 'border-gray-300'
+                                index < currentStep ? 'border-violet-600' : 'border-gray-300'
                             } mx-4"></div>
                         )}
                     </div>
